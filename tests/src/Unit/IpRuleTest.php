86a518c11110fabf4add14cb3071e4810e8c96db
Steve Worley <steven.worley@acquia.com>
2016-09-19 10:20:47 +1000

    Adding unit test cases

---
+++ tests/src/Unit/IpRuleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Constains namespace Drupal\Tests\restrict\Unit\IpRuleTest;
+ */
+
+namespace Drupal\Tests\restrict\Unit;
+
+use Drupal\restrict\Rules\PathRule;
+
+/**
+ * @coversDefaultClass \Drupal\restrict\IpRule
+ */
+class IpRuleTest extends UnitTestCase {
+
+}
---
+++ tests/src/Unit/PathRuleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Constains Drupal\Tests\restrict\Unit\PathRuleTest;
+ */
+
+namespace Drupal\Tests\restrict\Unit;
+
+use Drupal\restrict\Rules\PathRule;
+
+/**
+ * @coversDefaultClass \Drupal\restrict\PathRule
+ */
+class PathRuleTest extends UnitTestCase {
+}
